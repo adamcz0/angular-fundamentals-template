7fa361582d4d9873dabd7207fba5e6bc5992b1a0
Czifra Adam <czifra.adam1@gmail.com>
2024-09-29 21:04:45 +0200

    Angular router and services no.3

--- src/app/services/courses-store.service.ts
+++ src/app/services/courses-store.service.ts
@@ -1,42 +1,145 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CoursesService } from './courses.service';
+import { BehaviorSubject, catchError, finalize, Observable, tap, map, subscribeOn} from 'rxjs';
+
+interface Course {
+    title: string,
+    description: string,
+    duration: number,
+    authors: string[]
+}
 
 @Injectable({
     providedIn: 'root'
 })
 export class CoursesStoreService {
+
+    constructor(private coursesService: CoursesService) {}
+
+    private courses$$ = new BehaviorSubject<Course[]>([]);
+    private isLoading$$ = new BehaviorSubject<boolean>(false);
+    public courses$: Observable<Course[]> = this.courses$$.asObservable();
+    public isLoading$: Observable<boolean> = this.isLoading$$.asObservable();
+
+    setLoading(value: boolean) {
+        this.isLoading$$.next(value);
+    }
+
     getAll(){
         // Add your code here
+        this.setLoading(true);
+        this.coursesService.getAll().pipe(
+            map((response: any) => response.courses as Course[]),
+            tap(courses => this.courses$$.next(courses)),
+            catchError(error => {
+                console.error('Error loading courses', error);
+                return [];
+            }),
+            finalize(() => this.setLoading(false))
+        )
+        .subscribe();
     }
 
     createCourse(course: any) { // replace 'any' with the required interface
         // Add your code here
+        this.setLoading(true);
+        this.coursesService.createCourse(course).pipe(
+            tap(() => this.getAll()),
+            catchError(error => {
+                console.error('Error creating course', error);
+                return [];
+            }),
+            finalize(() => this.setLoading(false))
+        ).subscribe();
     }
 
     getCourse(id: string) {
         // Add your code here
+        this.setLoading(true);
+        this.coursesService.getCourse(id).pipe(
+            catchError(error => {
+                console.error('Error loading course', error);
+                throw error;
+            }),
+            finalize(() => this.setLoading(false))
+        );
     }
 
-    editCourse(id: string, course: any) { // replace 'any' with the required interface
+    editCourse(id: string, course: Course) { // replace 'any' with the required interface
         // Add your code here
+        this.setLoading(true);
+        this.coursesService.editCourse(id, course).pipe(
+            tap(() => this.getAll()),
+            catchError(error => {
+                console.error('Error editing course', error);
+                return [];
+            }),
+            finalize(() => this.setLoading(false))
+        ).subscribe();
     }
 
     deleteCourse(id: string) {
         // Add your code here
+        this.setLoading(true);
+        this.coursesService.deleteCourse(id).pipe(
+            tap(() => this.getAll()),
+            catchError(error => {
+                console.error('Error deleting course', error);
+                return [];
+            }),
+            finalize(() => this.setLoading(false))
+        ).subscribe();
     }
 
     filterCourses(value: string) {
         // Add your code here
+        this.setLoading(true);
+        this.coursesService.filterCourses(value).pipe(
+            map((response: any) => response.courses as Course[]),
+            tap(courses => this.courses$$.next(courses)),
+            catchError(error => {
+                console.error('Error filtering courses', error);
+                return [];
+            }),
+            finalize(() => this.setLoading(false))
+        ).subscribe();
     }
 
     getAllAuthors() {
         // Add your code here
+        this.setLoading(true);
+        this.coursesService.getAllAuthors().pipe(
+            tap(authors => console.log('Authors: ', authors)),
+            catchError(error => {
+                console.error('Error loading authors', error);
+                return [];
+            }),
+            finalize(() => this.setLoading(false))
+        ).subscribe();
     }
 
     createAuthor(name: string) {
         // Add your code here
+        this.setLoading(true);
+        this.coursesService.createAuthor(name).pipe(
+            tap(() => this.getAllAuthors()),
+            catchError(error => {
+                console.error('Error creating author', error);
+                return [];
+            }),
+            finalize(() => this.setLoading(false))
+        ).subscribe();
     }
 
     getAuthorById(id: string) {
         // Add your code here
+        this.setLoading(true);
+        this.coursesService.getAuthorById(id).pipe(
+            catchError(error => {
+                console.error('Error creating author', error);
+                throw error;
+            })
+        );
     }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,42 +1,65 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+interface Course {
+    title: string,
+    description: string,
+    duration: number,
+    authors: string[]
+}
 
 @Injectable({
     providedIn: 'root'
 })
 export class CoursesService {
+
+    constructor(private http: HttpClient) {}
+
+    private baseCoursesUrl = 'http://localhost:4000/courses';
+    private baseAuthorsUrl = 'http://localhost:4000/authors';
+
     getAll() {
         // Add your code here
+       return this.http.get(`${this.baseCoursesUrl}/all`)
     }
 
-    createCourse(course: any) { // replace 'any' with the required interface
+    createCourse(course: Course) { // replace 'any' with the required interface
         // Add your code here
+        return this.http.post(`${this.baseCoursesUrl}/add`, course)
     }
 
-    editCourse(id: string, course: any) { // replace 'any' with the required interface
+    editCourse(id: string, course: Course) { // replace 'any' with the required interface
         // Add your code here
+        return this.http.put(`${this.baseCoursesUrl}/${id}`, course);
     }
 
     getCourse(id: string) {
         // Add your code here
+        return this.http.get(`${this.baseCoursesUrl}/${id}`);
     }
 
     deleteCourse(id: string) {
         // Add your code here
+        return this.http.delete(`${this.baseCoursesUrl}/${id}`);
     }
 
     filterCourses(value: string) {
         // Add your code here
+        return this.http.get(`${this.baseCoursesUrl}?filter=${value}`);
     }
 
     getAllAuthors() {
         // Add your code here
+        return this.http.get(`${this.baseAuthorsUrl}/all`);
     }
 
     createAuthor(name: string) {
         // Add your code here
+        return this.http.post(`${this.baseAuthorsUrl}/add`, name);
     }
 
     getAuthorById(id: string) {
         // Add your code here
+        return this.http.get(`${this.baseAuthorsUrl}/${id}`);
     }
 }
